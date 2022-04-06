54d0a030da9322b30e986ae8a36fb87dc1e08fe9
jintfs <jintfs@outlook.jp>
2022-04-06 15:04:04 +0900

    Add test case

--- test.spec.js
+++ test.spec.js
@@ -2,7 +2,7 @@ describe('Default Test Set', () => {
     it('test1 should be passed', () => {
         console.log('test1 passed')
     });
-    
+
     it('test2 should be passed', () => {
         console.log('test2 passed')
     });
